1a7f9132fd55ff5060467f7f2c0cfa161d5ca660
4thMemorize <binu2893@naver.com>
2019-04-08 03:33:05 +0900

    Final 1

--- moniter.php
+++ moniter.php
@@ -9,62 +9,57 @@
     <?php
     $Serial = "$_GET[Serial]";
 
+    date_default_timezone_set("Asia/Seoul");
+    $month= date("n");
+    $day = date("j");
+    $hour = date("H");
+    $min = date("i");
+    $timestamp = date("H:i:s");
+
     $link=mysqli_connect("localhost", "root", "4thMemorize", "GW");
     mysqli_set_charset($link, "utf8");
 
-    if (!($_GET['Serial']&$_GET['Grade']&$_GET['Class']&$_GET['Number']&$_GET['Name'])) {
-      $comment = "누락된 정보가 있습니다. 확인해 주세요.";
-    }
 
-    else {
-          $sql="SELECT * FROM Identify WHERE Serial='$Serial'";
-        $result=mysqli_query($link, $sql) or die ("Error: " .mysqli_error($link));
+      $sql="SELECT * FROM Identify WHERE Serial='$Serial'";
+      $result=mysqli_query($link, $sql) or die ("Error: " .mysqli_error($link));
 
-        $Inform = mysqli_fetch_array($result);
-        $ID = $Inform['ID'];
-        $Student_Inform =array(
-          '1' => $Inform['Grade'],
-          '2' => $Inform['Class'],
-          '3' => $Inform['Number'],
-          '4' => $Inform['Name']
-        );
+      $Inform = mysqli_fetch_array($result);
+      $ID = $Inform['ID'];
+      $Student_Inform =array(
+        '1' => $Inform['Grade'],
+        '2' => $Inform['Class'],
+        '3' => $Inform['Number'],
+        '4' => $Inform['Name']
+      );
 
-        date_default_timezone_set("Asia/Seoul");
+      if(!(isset($ID))) {
+        $comment = "카드 고유번호와 일치하는 정보가 없습니다.";
+      }
 
-        $month= date("n");
-        $day = date("j");
-        $hour = date("H");
-        $min = date("i");
-        $timestamp = date("H:i:s");
+      else {
 
-
-        if ($hour>8)
-        {
-          if ($min>3)
-          {
-            $query = "지각 처리되었습니다.";
-            $log_txt = $timestamp."     ". $Student_Inform['1']."학년 ". $Student_Inform['2']."반 ". $Student_Inform['3']."번 ". $Student_Inform['4']. "   ". "불출석 처리 (사유 : 지각)";
-          }
+        if ($hour>8 & $min>1) {
+          $query = "지각 처리되었습니다.";
+          $log_txt = $timestamp."     ". $Student_Inform['1']."학년 ". $Student_Inform['2']."반 ". $Student_Inform['3']."번 ". $Student_Inform['4']. "   ". "불출석 처리 (사유 : 지각)";
         }
-        else
-        {
+        else {
           $query = '입실 처리되었습니다.';
           $record_sql = "UPDATE ".$month."월 SET ".$day."일 = '출석' WHERE ID='$ID';";
           $record_result=mysqli_query($link, $record_sql) or die ("Error:".mysqli_error($link));
 
           $log_txt = $timestamp."     ". $Student_Inform['1']."학년 ". $Student_Inform['2']."반 ". $Student_Inform['3']."번 ". $Student_Inform['4']."   ". "입실 처리";
+          $comment = $Student_Inform['1']."학년".$Student_Inform['2']."반".$Student_Inform['3']."번".$Student_Inform['4'];
         }
 
         $log_dir = ".";
         $log_file = fopen($log_dir."./log/php.log", "a");
         fwrite($log_file, $log_txt."\r\n");
         fclose($log_file);
-    }
-
+      }
 
     ?>
     <h2 id = 'Student_ID'>
-      <?=$Student_Inform['1']?>학년 <?=$Student_Inform['2']?>반 <?=$Student_Inform['3']?>번 <?=$Student_Inform['4']?></br>
+      <?=$comment?>
       <div id='query'>
         <?=$query?>
       </div>
