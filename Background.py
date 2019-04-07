a7e4264f0aa8d65098b2501b21b3a3076f2af1d2
4thMemorize <42008128+4thMemorize@users.noreply.github.com>
2019-04-08 08:36:14 +0900

    Update Background.py
    rollback Background

--- Background.py
+++ Background.py
@@ -35,6 +35,7 @@ if RecentMonth_value != month:
 
     cur.execute("""ALTER TABLE `{month}월`
                 MODIFY COLUMN ID int(3) PRIMARY KEY
+                AUTO_INCREMENT
                 """.format(month=time.month))
     link.commit()
     filewrite = open('./log/Back.conf', mode='w')
